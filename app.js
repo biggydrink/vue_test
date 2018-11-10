0806cbfc91c8b800cbbfafa47845e483f251acb8
biggydrink <andrew.butz@gmail.com>
2018-11-10 06:44:51 -0600

    added title, dogs - removed hover test

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+let mainTitle = new Vue({
+    el: '#main_title',
+    data: {
+        title: 'Vue Tests'
+    }
+});
+
 let text = new Vue({
     el: '#vue_text',
     data: {
@@ -15,16 +22,54 @@ let list = new Vue({
             { text: 'Kittens'},
             { text: '고양이'},
             { text: '고양이 새끼'}
-        ]
+        ],
+        cats: true,
+        buttonName: 'Dogs',
+        imgSrc: "To-infinity-and-beyond.jpeg",
+        imgAlt: "cat in cardboard spaceship",
+        imgTitle: "You, my friend, are responsible for delaying my rendezvous with star command!",
+    },
+
+    methods: {
+        swap: function() {
+            if (this.cats) {
+                this.myList = [
+                    {text: 'Doggo'}, 
+                    {text: 'Pup'}, 
+                    {text: 'Dogs'}, 
+                    {text: 'Doge'},
+                    {text: '개'}
+                ];
+                this.buttonName = "Cats";
+                this.imgSrc = "codedoge.jpg";
+                this.imgAlt = "doge meme - coding: <doge>, hello world, much intelligent, many html, wow how skill, code doge thx, very computer, such programming, wow";
+                this.imgTitle = "such title";
+                this.cats = false;
+                console.log('cats was true')
+            } else {
+                this.myList = [
+                    { text: 'Kitty'},
+                    { text: 'Cat'},
+                    { text: 'Kitten'},
+                    { text: '고양이'},
+                    { text: '고양이 새끼'}
+                ];
+                this.buttonName = "Dogs";
+                this.imgSrc = "To-infinity-and-beyond.jpeg";
+                this.imgAlt = "cat in cardboard spaceship";
+                this.imgTitle = "You, my friend, are responsible for delaying my rendezvous with star command!!";
+                this.cats = true;
+                console.log('cats was false')
+            }
+        },
+        reverse: function() {
+            for (i = 0; i < list.myList.length; i++ ) {
+                list.myList[i].text = list.myList[i].text.split('').reverse().join('');
+            }
+        }
     }
 });
 
-let hover = new Vue({
-    el: '#vue_hover_text',
-    data: {
-        title: 'You, my friend, are responsible for delaying my rendezvous with star command!'
-    }
-});
 
 let dynamic = new Vue({
     el: '#dynamo',
