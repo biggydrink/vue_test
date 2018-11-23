e9215d7e8b7d05a11f1dd428c9d2326d5a55c285
biggydrink <andrew.butz@gmail.com>
2018-11-23 17:32:11 -0600

    added comparison non-component lists

--- components.js
+++ components.js
@@ -9,7 +9,15 @@ let firstComp = new Vue({
         groceryList: [
             { id: 0, text: 'Eggplant' },
             { id: 1, text: 'Cheddar' },
-            { id: 2, text: 'Kimchi' }
+            { id: 2, text: 'Kimchi'},
+        ],
+        tags: [
+            { text: '<todo-item> [name of component]' }
+        ],
+        bindings: [
+            { text: 'v-for="item in groceryList"' },
+            { text: 'v-bind:todo="item"' },
+            { text: 'v-bind:key="item.id" [optional?]' }
         ]
     }
 })
@@ -18,5 +26,26 @@ let siteTitle = new Vue({
     el: '#site_title',
     data: {
         title: 'Components testing'
+    }
+})
+
+let notComp = new Vue({
+    el: '#not_comp',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Tortillas' },
+            { id: 1, text: 'Taco seasoning' },
+            { id: 2, text: 'Jalapenos' },
+            { id: 3, text: 'Black beans' },
+            { id: 4, text: 'Salsa' },
+            { id: 5, text: 'Tomatoes' },
+            { id: 6, text: 'Cabbage' }
+        ],
+        tags: [
+            '<li>'
+        ],
+        bindings: [
+            'v-for'
+        ]
     }
 })
